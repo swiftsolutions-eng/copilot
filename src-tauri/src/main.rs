1dee263a5dd173620eae95baf55cdca9a81d40de
marquelzikri <zmarquel@outlook.com>
2022-07-27 08:36:20 +0700

    Enable keyboard copy paste

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,7 +6,35 @@
 use tauri::{Menu, MenuItem, Submenu};
 
 fn main() {
-    let menu = Menu::new().add_submenu(Submenu::new("appname", Menu::new().add_native_item(MenuItem::Quit)));
+    let about_menu = Submenu::new("App", Menu::new()
+      .add_native_item(MenuItem::Hide)
+      .add_native_item(MenuItem::HideOthers)
+      .add_native_item(MenuItem::ShowAll)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Quit));
+
+    let edit_menu = Submenu::new("Edit", Menu::new()
+      .add_native_item(MenuItem::Undo)
+      .add_native_item(MenuItem::Redo)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Cut)
+      .add_native_item(MenuItem::Copy)
+      .add_native_item(MenuItem::Paste)
+      .add_native_item(MenuItem::SelectAll));
+
+    let view_menu = Submenu::new("View", Menu::new()
+      .add_native_item(MenuItem::EnterFullScreen));
+
+    let window_menu = Submenu::new("Window", Menu::new()
+      .add_native_item(MenuItem::Minimize)
+      .add_native_item(MenuItem::Zoom));
+
+    let menu = Menu::new()
+      .add_submenu(about_menu)
+      .add_submenu(edit_menu)
+      .add_submenu(view_menu)
+      .add_submenu(window_menu);
+
     app::fix_path().unwrap();
     tauri::Builder::default()
         .menu(menu)
